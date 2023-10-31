1b7db940b3a5ef35891ac065ca69bc98877cb0a6
Michael L. Collard <collard@uakron.edu>
2023-10-31 14:50:04 -0400

    Add sort for 2-element vector in reverse

--- sort.cpp
+++ sort.cpp
@@ -7,6 +7,14 @@
 #include "sort.hpp"
 
 // sort the entire vector in place in ascending order
-void sort(std::vector<int>&) {
+void sort(std::vector<int>& v) {
 
+    if (v.size() < 2)
+        return;
+
+    if (v[0] > v[1]) {
+        auto t = v[0];
+        v[0] = v[1];
+        v[1] = t;
+    }
 }
--- sortTest.cpp
+++ sortTest.cpp
@@ -36,5 +36,20 @@ int main() {
         assert(v[0] == 1);
     }
 
+    {
+        std::vector<int> v{ 2, 1 };
+        assert(!v.empty());
+        assert(v.size() == 2);
+        assert(v[0] == 2);
+        assert(v[1] == 1);
+
+        sort(v);
+
+        assert(!v.empty());
+        assert(v.size() == 2);
+        assert(v[0] == 1);
+        assert(v[1] == 2);
+    }
+
     return 0;
 }
